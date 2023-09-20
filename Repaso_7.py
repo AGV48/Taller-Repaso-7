243b16cad0ba109de2c02f3ee9c567e0c0be50b0
Alejandro Gutierrez <agutierrez289@soyudemedellin.edu.co>
2023-09-19 22:40:04 -0500

    Firts commit

---
+++ Repaso_7.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass
+
+@dataclass
+class Elemento:
+    nombre: str = ''
+
+    def __eq__(self, other) -> bool:
+        return self.nombre == other.nombre
+    
+    def __str__(self) -> str:
+        return self.nombre
+        
+class Conjunto:
+
+    Contador:int = 0
+    
+    def __init__(self, nombre) -> None:
+        self.lista_elementos: list = []
+        self.nombre: str = nombre
+        Conjunto.Contador += 1
+        self.__id: int = Conjunto.Contador
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    def contiene(self, obj: Elemento) -> bool:
+        for elemento in self.lista_elementos:
+            return elemento == obj
+        return False
+    
+    def agregar_elemento(self, obj: Elemento):
+        if not self.contiene(obj):
+            self.lista_elementos.append(obj)
+
+    def __add__(self, other):
+        nuevo_conjunto = Conjunto(f"{self.nombre} UNIDO {other.nombre}")
+        for elemento in self.lista_elementos:
+            nuevo_conjunto.agregar_elemento(elemento)
+        for elemento in other.lista_elementos:
+            nuevo_conjunto.agregar_elemento(elemento)
+        return nuevo_conjunto
+    
+
+    def intersectar(cls, c1, c2):
+        interseccion = [elemento for elemento in c1.lista_elementos if c2.lista_elementos.contiene(elemento)]
+        conjunto_interseccion = Conjunto(f"{c1.nombre} INTERSECTADO {c2.nombre}")
+        conjunto_interseccion.elementos = interseccion
+        return conjunto_interseccion
+
+    def __str__(self):
+        elementos_str = ', '.join(elemento.nombre for elemento in self.lista_elementos)
+        return f"({elementos_str})"
---
+++ app.py
@@ -0,0 +1,40 @@
+from Repaso_7 import Elemento, Conjunto
+
+nombre1 = Elemento(nombre="A")
+nombre2 = Elemento(nombre="B")
+nombre3 = Elemento(nombre="C")
+nombre4 = Elemento(nombre="D")
+nombre5 = Elemento(nombre="D")
+
+comparar = nombre1 == nombre2
+print(comparar)
+
+conjunto = Conjunto(nombre="Conjunto AA")
+print(conjunto.Contador, conjunto.nombre)
+print(conjunto.id)
+
+conjunto2 = Conjunto(nombre="Conjunto BB")
+print(conjunto2.Contador, conjunto2.nombre)
+print(conjunto2.id)
+
+
+print(conjunto.lista_elementos)
+conjunto.agregar_elemento(nombre1)
+print(conjunto.lista_elementos)
+conjunto.agregar_elemento(nombre3)
+print(conjunto.lista_elementos)
+
+print(conjunto2.lista_elementos)
+conjunto2.agregar_elemento(nombre2)
+print(conjunto2.lista_elementos)
+conjunto2.agregar_elemento(nombre4)
+print(conjunto2.lista_elementos)
+conjunto2.agregar_elemento(nombre5)
+print(conjunto2.lista_elementos)
+
+lista = conjunto + conjunto2
+print(lista)
+
+conjunto.intersectar(conjunto, conjunto2)
+
+
